94e5d7b9cbe567be346b52d671f171b1ded2f4b7
Takashi Mizohata <beatak@gmail.com>
2019-12-30 15:08:59 -0800

    Add release prepare script

---
+++ create_release_asset.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+VERSION="$1"
+
+if [[ -z $VERSION ]]; then
+  echo "** YOU NEED TO PASS VERSION STRING **"
+  exit 1
+fi
+
+TAR_FILE=$VERSION.tar
+
+tar cfv $TAR_FILE \
+  --exclude=".git*" \
+  --exclude="test" \
+  --exclude="README.md" \
+  --exclude=".DS_Store" \
+  --exclude="*.tar" \
+  --exclude="*.tar.gz" \
+  --exclude="create_release_asset.sh" \
+  .
+
+gzip $TAR_FILE
